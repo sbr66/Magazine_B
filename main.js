cb162e08ee22f7492128cd5bab38362ba83e4f8f
sbr66 <bonam22@gmail.com>
2023-01-10 12:53:26 +0900

    반응형 767px Shop Section

--- main.js
+++ main.js
@@ -117,19 +117,46 @@ shopTitles.forEach((title, idx) => {
 // shopContainer.appendChild(shopContentsClone);
 // shopSection.appendChild(shopContainer);
 
-for (let i = 0; i < 3; i++) {
-  const container = document.querySelector(".shop-container").cloneNode();
-  const shopContentsClone = document
-    .querySelector(".shop-contents")
-    .cloneNode();
-  container.appendChild(shopContentsClone);
-  shopSection.appendChild(container);
-}
+//
+window.addEventListener("resize", function () {
+  const ww = window.innerWidth;
+  if (ww <= 767) {
+    console.log("true");
+    for (let i = 0; i < 3; i++) {
+      const container = document.querySelector(".shop-container").cloneNode();
+      const shopText = document
+        .querySelector(".shop-container > p")
+        .cloneNode(true);
+      const shopContentsClone = document
+        .querySelector(".shop-contents")
+        .cloneNode();
+      container.appendChild(shopText);
+      container.appendChild(shopContentsClone);
+      shopSection.appendChild(container);
+    }
+    const shopContainer = document.querySelectorAll(".shop-container");
+    const shopBox = document.querySelectorAll(".shop-contents");
 
-const shopContainer = document.querySelectorAll(".shop-container");
-const shopTitle = document.querySelectorAll(".shop-title > h2");
-const shopPreviewBox = document.querySelectorAll(".shop-preview-box");
-console.log(shopContainer);
+    shopBox.forEach((box, idx) => {
+      const shopTitle = document.querySelectorAll(".shop-title > h2");
+      const shopContent = document.querySelectorAll(".shop-preview-box");
+
+      for (let i = 0; i < shopTitle.length; i++) {
+        box.append(shopTitle[idx]);
+      }
+      for (let i = 0; i < shopContent.length; i++) {
+        box.append(shopContent[idx]);
+      }
+    });
+
+    console.log(shopContainer);
+    shopContainer[1].style.background = "rgb(249, 231, 159)";
+    shopContainer[2].style.background = "rgb(239, 219, 228)";
+    shopContainer[3].style.background = "rgb(217, 228, 241)";
+  } else {
+    console.log("false");
+  }
+});
 
 // Books Section Tap
 bookTitles.forEach((title, idx) => {
