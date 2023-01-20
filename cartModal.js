5bd232b51fd994df5581ad8c73fb968deb200bc2
sbr66 <bonam22@gmail.com>
2023-01-20 11:53:35 +0900

    cart modal 반응형 완료

--- cartModal.js
+++ cartModal.js
@@ -1,6 +1,8 @@
 const cartBtn = document.querySelector("#header .cart");
 const cartCloseBtn = document.querySelector(".cart-close");
 const cartModal = document.querySelector(".cart-modal");
+const cartTop = document.querySelector(".cart-top");
+const cartModalBack = document.querySelector(".cart-modal-back");
 const body = document.querySelector(".wrapper");
 
 body.addEventListener("click", cartModalToggle);
@@ -8,15 +10,16 @@ body.addEventListener("click", cartModalToggle);
 let cartModalOpen = false;
 
 function cartModalToggle(e) {
+  console.log(e.target);
   if (e.target == cartBtn) {
     cartModal.style.transform = "translateX(0)";
     return (cartModalOpen = true);
   }
   if (cartModalOpen == true) {
-    if (e.target != cartModal) {
+    if (e.target != cartModal && e.target != cartTop) {
       cartModal.style.transform = "translateX(+101%)";
     } else {
-      if (e.target == cartCloseBtn) {
+      if (e.target == cartCloseBtn || e.target == cartModalBack) {
         cartModal.style.transform = "translateX(+101%)";
       }
     }
