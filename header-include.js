02c73ed9d6be1119987d259a8102e281deabce00
B <bonam22@gmail.com>
2023-01-26 15:44:15 +0900

    헤더 페이지별 디자인 변경

--- header-include.js
+++ header-include.js
@@ -10,4 +10,35 @@ fetch("header.html")
     menuBtn.addEventListener("click", () => {
       wrapper.classList.toggle("active");
     });
+
+    // 페이지별 헤더 디자인 변경
+    const url = window.location.pathname;
+    if (!url.includes("index")) {
+      const li = document.querySelectorAll("#header li");
+      const aLi = document.querySelectorAll("#header li a");
+      const header = document.querySelector("#header");
+      const headerLogo = document.querySelector("#header .left .logo img");
+      const languageLogo = document.querySelector(
+        "#header .right-list .language img"
+      );
+
+      const section = document.querySelector("section");
+      const sectionStyle = getComputedStyle(section);
+      const sectionBackground = sectionStyle.backgroundColor;
+      console.log(sectionBackground);
+
+      li.forEach((item) => {
+        item.style.color = "#222";
+      });
+
+      aLi.forEach((item) => {
+        item.style.color = "#222";
+      });
+
+      headerLogo.setAttribute("src", "/images/header_logo.svg");
+      languageLogo.setAttribute("src", "/images/asset5.png");
+
+      header.style.backdropFilter = "none";
+      header.style.backgroundColor = sectionBackground;
+    }
   });
