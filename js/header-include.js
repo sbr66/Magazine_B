8adb6585926f8a2b56efaac9059685558e416236
B <bonam22@gmail.com>
2023-02-13 21:59:54 +0900

    cart modal 상품 수량 변경 기능

--- js/header-include.js
+++ js/header-include.js
@@ -177,11 +177,35 @@ fetch("/magazine_b/header.html")
       
               <div class="cart-info-box">
                 <p class="cart-title">${list.cart_name}</p>
-                <p>￦${list.cart_sum}</p>
+                <p class="list-item-price">￦${list.cart_sum}</p>
                 <div class="cart-qnts">
-                  <i class="ri-subtract-line down-btn"></i>
+                  <form onsubmit="return false" class="sub-cart-form">                 
+                    <button type="submit" class="down-btn" id="down-btn-${list.cart_idx}">
+                      <i class="ri-subtract-line"></i>
+                    </button>
+                    <input type="hidden" name="cart_list_idx" value="${list.cart_idx}">
+                    <input type="hidden" name="cart_list_name" value="${list.cart_name}">
+                    <input type="hidden" name="cart_list_desc" value="${list.cart_desc}">
+                    <input type="hidden" name="cart_list_price" value="${list.cart_price}">
+                    <input type="hidden" name="cart_list_img" value="${list.cart_img}">
+                    <input type="hidden" name="cart_list_count" value="1" class="list-down-count">
+                    <input type="hidden" name="cart_list_sum" value="${list.cart_sum}" class="list-down-sum">                  
+                  </form>
+
                   <p class="cart-modal-qnt" id="cart-count-${list.cart_idx}">${list.cart_count}</p>
-                  <i class="ri-add-line up-btn"></i>
+
+                  <form onsubmit="return false" class="add-cart-form">                  
+                    <button type="submit" class="up-btn" id="up-btn-${list.cart_idx}">
+                      <i class="ri-add-line"></i>
+                    </button>
+                    <input type="hidden" name="cart_list_idx" value="${list.cart_idx}">
+                    <input type="hidden" name="cart_list_name" value="${list.cart_name}">
+                    <input type="hidden" name="cart_list_desc" value="${list.cart_desc}">
+                    <input type="hidden" name="cart_list_price" value="${list.cart_price}">
+                    <input type="hidden" name="cart_list_img" value="${list.cart_img}">
+                    <input type="hidden" name="cart_list_count" value="1" class="list-up-count">
+                    <input type="hidden" name="cart_list_sum" value="${list.cart_sum}" class="list-up-sum">                  
+                  </form>  
                 </div>
               </div>
 
@@ -213,12 +237,13 @@ fetch("/magazine_b/header.html")
         // console.log(cartSumPrice);
         checkOutBtn.textContent = `￦${cartSumPrice} CHECK OUT`;
 
-        // 상품별 수량 변경하기
+        // 장바구니에 담긴 상품 수량 및 합산가격 변경하기
         const cartModalUpBtns = document.querySelectorAll(".cart-qnts .up-btn");
         const cartModalDownBtns = document.querySelectorAll(
           ".cart-qnts .down-btn"
         );
         const cartModalQntEl = document.querySelectorAll(".cart-modal-qnt");
+        const listItemPriceEl = document.querySelectorAll(".list-item-price");
 
         let cartModalQnt = [];
         cartModalQntEl.forEach((qnt) => {
@@ -228,16 +253,112 @@ fetch("/magazine_b/header.html")
 
         cartModalUpBtns.forEach((btn, idx) => {
           btn.addEventListener("click", () => {
+            // + 버튼 클릭했을때 수량 및 합산 가격 변경
+            const upQntInputEl = document.querySelectorAll(".list-up-count"); // <input name="cart_list_count">
+            const upPriceInputEl = document.querySelectorAll(".list-up-sum"); // <input name="cart_list_sum">
+
             cartModalQnt[idx]++;
-            cartModalQntEl[idx].textContent = cartModalQnt[idx];
+            cartModalQntEl[idx].textContent = cartModalQnt[idx]; // 변경된 수량 출력
+            listItemPriceEl[idx].textContent = `￦${
+              Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price)
+            }`; // 변경된 가격 출력
+
+            upQntInputEl[idx].value = cartModalQnt[idx]; // + 버튼 눌렀을때 <input name="cart_list_count">의 value 변경
+            upPriceInputEl[idx].value =
+              Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price); // + 버튼 눌렀을때 <input name="cart_list_sum">의 value 변경
+
+            // console.log(listItemPriceEl[idx].textContent);
+            // console.log(upPriceInputEl[idx].value);
+
+            const addFormEl = document.querySelectorAll(".add-cart-form");
+            const addFormData = new FormData(addFormEl[idx]);
+            addFormData.set("cart_list_count", `${cartModalQnt[idx]}`);
+            addFormData.set(
+              "cart_list_sum",
+              `${Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price)}`
+            );
+            fetch(
+              `/magazine_b_back/cart_ctrl.php?req_cart=patch_cart&cart_idx=${idx}`,
+              {
+                method: "POST",
+                body: addFormData,
+              }
+            )
+              .then((res) => res.json())
+              .then((updateCart) => {
+                // console.log(updateCart);
+                // console.log(updateCart.cart_list);
+
+                let cartPriceSum = 0;
+                updateCart.cart_list.forEach((i) => {
+                  // console.log(i.cart_sum);
+                  cartPriceSum += Number(i.cart_sum);
+                });
+                checkOutBtn.textContent = `￦${cartPriceSum} CHECK OUT`; // 상품 수량 변경 후 장바구니 상품들의 가격 합계 출력
+
+                let cartCountSum = 0;
+                updateCart.cart_list.forEach((i) => {
+                  cartCountSum += Number(i.cart_count);
+                });
+                cartCountNum.forEach((num) => {
+                  num.textContent = `[${cartCountSum}]`;
+                }); // 상품 수량 변경 후 장바구니 상품들의 수량 합계 출력
+              });
           });
         });
 
         cartModalDownBtns.forEach((btn, idx) => {
           btn.addEventListener("click", () => {
+            // - 버튼 클릭했을때 수량 및 합산 가격 변경
+            const downQntInputEl =
+              document.querySelectorAll(".list-down-count"); // <input name="cart_list_count">
+            const downPriceInputEl =
+              document.querySelectorAll(".list-down-sum"); // <input name="cart_list_sum">
             if (cartModalQnt[idx] != 1) {
               cartModalQnt[idx]--;
-              cartModalQntEl[idx].textContent = cartModalQnt[idx];
+              cartModalQntEl[idx].textContent = cartModalQnt[idx]; // 변경된 수량 출력
+              listItemPriceEl[idx].textContent = `￦${
+                Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price)
+              }`; // 변경된 가격 출력
+
+              downPriceInputEl[idx].value =
+                Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price); // - 버튼 눌렀을때 <input name="cart_list_sum">의 value 변경
+              downQntInputEl[idx].value = cartModalQnt[idx]; // - 버튼 눌렀을때 <input name="cart_list_count">의 value 변경
+
+              const subFormEl = document.querySelectorAll(".sub-cart-form");
+              const subFormData = new FormData(subFormEl[idx]);
+              subFormData.set("cart_list_count", `${cartModalQnt[idx]}`);
+              subFormData.set(
+                "cart_list_sum",
+                `${
+                  Number(cartModalQnt[idx]) * Number(cartData[idx].cart_price)
+                }`
+              );
+              fetch(
+                `/magazine_b_back/cart_ctrl.php?req_cart=patch_cart&cart_idx=${idx}`,
+                {
+                  method: "POST",
+                  body: subFormData,
+                }
+              )
+                .then((res) => res.json())
+                .then((updateCart) => {
+                  // console.log(updateCart);
+                  let cartPriceSum = 0;
+                  updateCart.cart_list.forEach((i) => {
+                    // console.log(i.cart_sum);
+                    cartPriceSum += Number(i.cart_sum);
+                  });
+                  checkOutBtn.textContent = `￦${cartPriceSum} CHECK OUT`; // 상품 수량 변경 후 장바구니 상품들의 가격 합계 출력
+
+                  let cartCountSum = 0;
+                  updateCart.cart_list.forEach((i) => {
+                    cartCountSum += Number(i.cart_count);
+                  });
+                  cartCountNum.forEach((num) => {
+                    num.textContent = `[${cartCountSum}]`;
+                  }); // 상품 수량 변경 후 장바구니 상품들의 수량 합계 출력
+                });
             }
           });
         });
@@ -245,90 +366,16 @@ fetch("/magazine_b/header.html")
         // 카트 상품 삭제
         const rmvCartBtn = document.querySelectorAll(".remove-cart");
         rmvCartBtn.forEach((btn, idx) => {
-          btn.addEventListener("click", function () {
+          btn.addEventListener("click", async function () {
             const cartIdx = Number(this.getAttribute("id").split("-")[1]);
             const listItem = document.querySelector(`#cart-list-${cartIdx}`);
-
-            // const rmvCartCount = document.querySelector(
-            //   `#cart-count-${cartIdx}`
-            // );
-
-            fetch(
-              `/magazine_b_back/cart_ctrl.php?req_cart=del_cart&cart_idx=${cartIdx}`
+            await fetch(
+              `/magazine_b_back/cart_ctrl.php?req_cart=del_cart&cart_idx=${idx}`
             )
               .then((res) => res.json())
               .then((delData) => {
-                // console.log(delData);
-                // if (delData.length === 0) {
-                //   cartModalWrapper.innerHTML = `<p class="no-cart">장바구니에 상품이 없습니다.</p>`;
-                //   cartCountNum.forEach((num) => {
-                //     num.textContent = "[0]";
-                //   });
-                //   return;
-                // }
-                // cartModalWrapper.innerHTML = null;
-                // delData.map((list) => {
-                //   cartListEl = `
-                //     <div class="cart-list" id="cart-list-${list.cart_idx}">
-                //       <div class="cart-img">
-                //         <img src="${list.cart_img}" alt="" />
-                //       </div>
-                //       <div class="cart-info-box">
-                //         <p class="cart-title">${list.cart_name}</p>
-                //         <p>￦${list.cart_sum}</p>
-                //         <div class="cart-qnts">
-                //           <i class="ri-subtract-line"></i>
-                //           <p class="cart-count" id="cart-count-${list.cart_idx}">${list.cart_count}</p>
-                //           <i class="ri-add-line"></i>
-                //         </div>
-                //       </div>
-                //       <i class="ri-close-line remove-cart" id="btn-${list.cart_idx}"></i>
-                //     </div>
-                //   `;
-                //   cartModalWrapper.innerHTML += cartListEl;
-                // });
-                // // 상품 삭제 후 총 수량 출력
-                // const cartListCountEl =
-                //   document.querySelectorAll(".cart-count");
-                // let cartListCount = 0;
-                // cartListCountEl.forEach((count) => {
-                //   // console.log(count.textContent);
-                //   cartListCount += Number(count.textContent);
-                // });
-                // // console.log(cartListCount);
-                // cartCountNum.forEach((num) => {
-                //   num.textContent = `[${cartListCount}]`;
-                // });
-                // // 상품 삭제 후 합산 가격 출력
-                // const checkOutBtn = document.querySelector(".check-out-btn a");
-                // let cartSumPrice = 0;
-                // delData.forEach((data) => {
-                //   cartSumPrice += Number(data.cart_sum);
-                // });
-                // // console.log(cartSumPrice);
-                // checkOutBtn.textContent = `￦${cartSumPrice} CHECK OUT`;
-                // =============================================
-                // alert(del.msg);
-                console.log("삭제 버튼 누른 상품의 데이터 : ", cartData[idx]);
-                listItem.style.display = "none"; // 삭제 버튼 누르면 cart modal에서 상품 안보이게
-                cartCountNum.forEach((num) => {
-                  num.textContent = `[${
-                    Number(num.textContent.replace(/[^0-9]/g, "")) -
-                    Number(cartData[idx].cart_count)
-                  }]`; // 상품 삭제 후 변경된 장바구니 상품 총 수량 출력(cart modal & header)
-
-                  if (num.textContent == "[0]") {
-                    cartModalWrapper.innerHTML = `<p class="no-cart">장바구니에 상품이 없습니다.</p>`;
-                    checkOutBtn.style.display = "none";
-                    return;
-                  } // 장바구니에 담긴 상품을 모두 삭제했을때 메세지 출력 & Check Out 버튼 안보이게
-                });
-
-                checkOutBtn.textContent = `￦${
-                  Number(checkOutBtn.textContent.replace(/[^0-9]/g, "")) -
-                  Number(cartData[idx].cart_sum)
-                }
-                 CHECK OUT`; // 상품 삭제 후 변경된 장바구니 상품 가격 합계 출력
+                alert("상품이 삭제되었습니다.");
+                location.reload();
               })
               .catch((err) => console.log(err));
           });
